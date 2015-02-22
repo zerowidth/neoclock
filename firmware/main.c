20c0d3818881a3c667147aa969daa85a969a4441
Nathan Witmer <nathan@zerowidth.com>
2015-02-22 14:33:27 -0700

    asm to zero out a string of pixels

--- firmware/main.c
+++ firmware/main.c
@@ -9,52 +9,51 @@
 
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
+#include <stdlib.h> /* for itoa */
 #include "softuart.h"
 
 #define PWM_DDR OC1B_DDR
 #define PWM_BIT OC1B_BIT
 #define PWM_COM_BIT COM1B1
 #define PWM_REGISTER OCR1B
-#define DELAY 10
 
-void check_serial()
+#define PIXELS 16
+#define PIXEL_PORT  PORTA  // Port of the pin the pixels are connected to
+#define PIXEL_DDR   DDRA   // Port of the pin the pixels are connected to
+#define PIXEL_BIT   PORTA0 // Bit of the pin the pixels are connected to
+static uint8_t xxx, yyy;
 {
-  char c;
-  if ( softuart_kbhit() ) {
-    c = softuart_getchar();
-    softuart_putchar('-');
-    softuart_putchar('>');
-    softuart_putchar('[');
-    softuart_putchar(c);
-    softuart_putchar(']');
-    softuart_putchar('\r');
-    softuart_putchar('\n');
+
+void zero() {
+  uint8_t i;
+  cli();
+  for(i=0; i < 96; i++) {
+    asm volatile(
+        "sbi %[port], %[pin]" "\n\t"
+        "nop"                 "\n\t"
+        "cbi %[port], %[pin]" "\n\t"
+        "nop"                 "\n\t"
+        "nop"                 "\n\t"
+        "nop"                 "\n\t"
+        "nop"                 "\n\t"
+        "nop"                 "\n\t"
+        "nop"                 "\n\t"
+        "nop"                 "\n\t"
+        ::
+        [port] "i" (_SFR_IO_ADDR(PIXEL_PORT)),
+        [pin] "i" (PIXEL_BIT));
   }
+  sei();
 }
 
 int main(void)
 {
+  PIXEL_DDR |= _BV(PIXEL_BIT);
+
   softuart_init();
   sei();
 
   softuart_puts_P( "\r\nready.\r\n" );
 
-  PWM_DDR |= _BV(PWM_BIT);
-  TCCR1A   |= _BV(PWM_COM_BIT) | _BV(WGM10);
-  TCCR1B   |= _BV(CS10) | _BV(WGM12);
-  PWM_REGISTER = 0;
-
-  while(1) {
-    while(PWM_REGISTER < 128) {
-      check_serial();
-      PWM_REGISTER++;
-      _delay_ms(DELAY);
-    }
-    while(PWM_REGISTER > 0) {
-      check_serial();
-      PWM_REGISTER--;
-      _delay_ms(DELAY);
-    }
-  }
   return 0;
 }
